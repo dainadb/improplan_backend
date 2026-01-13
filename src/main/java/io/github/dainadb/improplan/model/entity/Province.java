78a16302b4d854c3fbac46f7815bbffb5d520fa1
Daina Díaz <daina.97.db@gmail.com>
2026-01-13 20:39:30 +0100

    Entidades JPA y sus relaciones
    Configuración inicial del modelo de dominio principal utilizando entidades JPA. Este commit introduce las entidades fundamentales requeridas para la funcionalidad de la aplicación y establece las relaciones entre ellas.

--- src/main/java/io/github/dainadb/improplan/model/entity/Province.java
+++ src/main/java/io/github/dainadb/improplan/model/entity/Province.java
@@ -1,5 +1,68 @@
 package io.github.dainadb.improplan.model.entity;
 
-public class Province {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Entidad que representa una Provincia.
+ */
+@Entity
+@Table(name = "provinces")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Province implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Clave primaria de la provincia.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_province")
+    private Integer id;
+
+    /**
+     * Nombre de la provincia.
+     */
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    /**
+     * Latitud de la provincia.
+     */
+    private Double latitude;
+
+    /**
+     * Longitud de la provincia.
+     */
+    private Double longitude;
+
+    /**
+     * La comunidad autónoma a la que pertenece esta provincia.
+     * Relación unidireccional ManyToOne con AutonomousCommunity.
+     */
+    @ManyToOne(fetch = FetchType.LAZY) //Se usa el fetch Lazy para evitar cargar la comunidad autónoma al cargar la provincia, solo se cargará cuando se acceda al atributo. 
+    @JoinColumn(name = "id_autonomous_community", referencedColumnName = "id_autonomous")
+    private AutonomousCommunity autonomousCommunity;
+
+    //No se establece una relación bidireccional con Municipality para simplificar el modelo de datos.(lo mismo que se ha comentado en AutonomousCommunity)
+    
 }
